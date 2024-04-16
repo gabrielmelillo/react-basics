61edbf797ec75d4ad5001999c10a78fae21a050a
Gabriel Melillo <gabrielramosmelillo@hotmail.com>
2024-04-16 22:35:49 +0100

    Set up MUI material design UI library.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// React Imports
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Third-party Imports
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+// Own Imports
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
