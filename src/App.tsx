5badeb340c1ea3d35cd6e36774d45d28c68244e5
Gabriel Melillo <gabrielramosmelillo@hotmail.com>
2024-04-17 12:13:15 +0100

    Built the page layout and menu drawer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import Button from "@mui/material/Button";
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import MenuDrawer from "./MenuDrawer";
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Search
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <MenuDrawer />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Typography paragraph>Hello world</Typography>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
